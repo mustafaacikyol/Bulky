6758c10adac3fcc63c79dfac173dc99834077598
Mustafa Açıkyol <acikyolmustafa17@gmail.com>
2025-07-28 20:46:16 +0300

    Add Admin and Customer Areas

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using System.Diagnostics;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyBookWeb.Controllers
+namespace BulkyBookWeb.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
